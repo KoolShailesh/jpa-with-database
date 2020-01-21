71db8dc0eac7d8d3b1aab40a1c1ef702290cc452
Shailesh Chandra <shailesh.chandra@mytrusteddr.com>
2020-01-21 10:17:50 +0530

    updated

--- src/main/java/com/shailesh/JPAApplication.java
+++ src/main/java/com/shailesh/JPAApplication.java
@@ -1,8 +1,14 @@
 package com.shailesh;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.shailesh.misc.ProfileEntity;
+import com.shailesh.misc.ProfileRepository;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -12,4 +18,20 @@ public class JPAApplication {
 		SpringApplication.run(JPAApplication.class, args);
 	}
 
+	
+	@Autowired
+	private ProfileRepository profile;
+	
+	@PostConstruct
+	private void doSomething() {
+		
+		ProfileEntity profileEntity = new ProfileEntity();
+		
+		profileEntity.setId(1l);
+		profileEntity.setDescription("desc1");
+		
+		profile.save(profileEntity);
+		
+		
+	}
 }
---
+++ src/main/java/com/shailesh/misc/ProfileEntity.java
@@ -0,0 +1,35 @@
+package com.shailesh.misc;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "porta")
+public class ProfileEntity {
+    @Id
+    @Column(name = "idporta")
+    private Long id;
+    @Column(name = "title")
+    private String name;
+    @Column(name = "imageURL")
+    private String image;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "twitterName")
+    private String twitterName;
+    
+    @Column(name = "fbName")
+    private String fbName;
+    
+}
